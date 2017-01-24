03bbb00432b4728fc1c0fabbe9369c5191d6c4b8
mmsamiei <mohmahsamiei@gmail.com>
2017-01-25 01:10:45 +0330

    bash started

---
+++ bash/ReadMe.md
@@ -0,0 +1,9 @@
+#how to exequte it?
+
+#run followings:
+
+    chmod u+x script.sh
+
+    ls -l script.sh
+
+    script.sh
---
+++ bash/intro.sh
@@ -0,0 +1,15 @@
+echo Hello World
+
+variable="Samiei"
+
+# But Not like this
+# variable = "Samiei"
+
+#using variables
+echo $variable
+
+#paramete expansion
+#?
+
+
+

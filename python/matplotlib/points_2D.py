da96af2fee018a5fee79b6ebaade3e57a66a46dd
mmsamiei <mohmahsamiei@gmail.com>
2017-03-03 19:42:03 +0330

    Python-plot-point

---
+++ python/matplotlib/points_2D.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+horizontal_axis_data = [1,4,12,14,15,17,19,19,21,23,24,25]
+vertical_axis_data = [0,2,-1,7,9,-3,0,6,9,10,-2,3]
+plt.plot(horizontal_axis_data,vertical_axis_data,"ro")#ro = red circle ploints
+plt.show()
+plt.plot(horizontal_axis_data,vertical_axis_data)# without third argumant it
+    #draw  linear chart
+plt.show()

d5f38a775c2ae01f08c108df98915bce9d9fef3a
mmsamiei <mohmahsamiei@gmail.com>
2017-04-27 19:56:35 +0430

    simple-network

--- python/network/simplest/client.py
+++ python/network/simplest/client.py
@@ -2,8 +2,10 @@ import socket
 
 client_socket = socket.socket()
 host = socket.gethostname()
-port = 7
+port = 1376
 
-client_socket.connect((host,port))
+#client_socket.connect((host,port))
+client_socket.connect(('37.59.145.129',port))
 print client_socket.recv(1024)
 client_socket.close()
+

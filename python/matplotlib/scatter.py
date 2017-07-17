4d3650b72f926d57fe9fbc75dc25090b61ec00fa
mmsamiei <mohmahsamiei@gmail.com>
2017-07-17 16:14:35 +0430

    TensorFlow Go  to the hell KhorzoKhan

---
+++ python/matplotlib/scatter.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+horizontal_axis_data = [1,4,12,14,15,17,19,19,21,23,24,25]
+vertical_axis_data = [0,2,-1,7,9,-3,0,6,9,10,-2,3]
+plt.scatter(horizontal_axis_data,vertical_axis_data,color='red')#ro = red circle ploints
+plt.show()
+plt.clf() # for clear screen
+plt.plot(horizontal_axis_data,vertical_axis_data)# without third argumant it
+    #draw  linear chart
+plt.show()

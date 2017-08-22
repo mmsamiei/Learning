a4d87f3785c991e6e4929aff7a95dcaa230e5070
mmsamiei <mohmahsamiei@gmail.com>
2017-08-22 19:06:07 +0430

    improve some python tutorials according to PEP8 standard

--- python/introduction/classes.py
+++ python/introduction/classes.py
@@ -1,22 +1,23 @@
-#in the name of Allah
+# in the name of Allah
+# for define class we get subclass from object:
 
-
-#for define class we get subclass from object:
 
 class Human(object):
-    species = "H. sapiens"  #this is attribute
-    def __init__(self, name):   #this is a constructor
+    species = "H. sapiens"  # this is attribute
+
+    def __init__(self, name):   # this is a constructor
         self.name = name
         self.age = 0
-    def say(self, msg):     #this is a method
-        return "{self}: {msg}".format(self=self.name,msg=msg)
 
-    #an classmethod shared between all of instances
+    def say(self, msg):     # this is a method
+        return "{self}: {msg}".format(self=self.name, msg=msg)
+
+    # an classmethod shared between all of instances
     @classmethod
     def get_species(cls):
         return cls.species
 
-    #property acts like getter
+    # property acts like getter
     @property
     def age(self):
         return self._age
@@ -25,7 +26,7 @@ class Human(object):
     def age(self, age):
         self._age = age
 
-    #instantiate a class
+    # instantiate a class
 p = Human(name="parham")
 print p.say("hi")
 
@@ -33,5 +34,3 @@ i = Human(name="iman")
 print i.say("hello {friend}").format(friend=p.name)
 
 p.age = 20
-
-

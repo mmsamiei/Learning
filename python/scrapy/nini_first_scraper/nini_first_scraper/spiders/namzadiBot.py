b78b79bebb0df41188fc3c65b45da68821d0cf66
mmsamiei <mohmahsamiei@gmail.com>
2017-12-19 16:19:55 +0330

    first simple ninisite_org scraper for namzadi :)

---
+++ python/scrapy/nini_first_scraper/nini_first_scraper/spiders/namzadiBot.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class NamzadibotSpider(scrapy.Spider):
+    name = 'namzadiBot'
+    allowed_domains = ['https://www.ninisite.com/discussion/forum/115/%d9%86%d8%a7%d9%85%d8%b2%d8%af%db%8c']
+    start_urls = ['https://www.ninisite.com/discussion/forum/115/%d9%86%d8%a7%d9%85%d8%b2%d8%af%db%8c/']
+
+    def parse(self, response):
+        titles = response.css('.topic_subject::text').extract()
+        author = response.css('.last-topic-user a::text').extract()
+
+        for item in zip(titles, author):
+            scraped_info = {
+                    'titles':item[0],
+                    'author':item[1]
+                    }
+            yield scraped_info

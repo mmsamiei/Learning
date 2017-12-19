c7aafa19e6585c5034511ace43370557fd3df94a
mmsamiei <mohmahsamiei@gmail.com>
2017-12-19 13:46:28 +0330

    successful extract one page

--- python/scrapy/brickset-scraper/scraper.py
+++ python/scrapy/brickset-scraper/scraper.py
@@ -7,4 +7,14 @@ class BrickSetSpider(scrapy.Spider):
     def parse(self, response):
         SET_SELECTOR = '.set'
         for brickset in response.css(SET_SELECTOR):
-            pass
+            NAME_SELECTOR = 'h1 a ::text'
+            PIECES_SELECTOR = './/dl[dt/text() = "Pieces"]/dd/a/text()'
+            IMAGE_SELECTOR = 'img ::attr(src)'
+            yield{
+                    'name':brickset.css(NAME_SELECTOR)[-1].extract(),
+                    'pieces':brickset.xpath(PIECES_SELECTOR).extract_first(),
+                    'image':brickset.css(IMAGE_SELECTOR).extract_first()
+            }
+
+
+

f0f27845f5e60c4fc143d199477d2ab875a32519
mmsamiei <mohmahsamiei@gmail.com>
2017-12-19 13:54:49 +0330

    add successful scrap multiple pages

--- python/scrapy/brickset-scraper/scraper.py
+++ python/scrapy/brickset-scraper/scraper.py
@@ -16,5 +16,14 @@ class BrickSetSpider(scrapy.Spider):
                     'image':brickset.css(IMAGE_SELECTOR).extract_first()
             }
 
+            NEXT_PAGE_SELECTOR = '.next a ::attr(href)'
+            next_page = response.css(NEXT_PAGE_SELECTOR).extract_first()
+            if next_page:
+                yield scrapy.Request(
+                        response.urljoin(next_page),
+                        callback=self.parse
+                        # once you have gotten the HTML from this page pass it back to this method"
+                        )
+                # scrapy.Request means that "Hey! scrawl this page! :D"
 
 

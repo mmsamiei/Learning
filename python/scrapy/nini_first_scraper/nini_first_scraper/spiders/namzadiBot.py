2f1d8f27af5e12155833bff862588dbf9fb83a68
mmsamiei <mohmahsamiei@gmail.com>
2017-12-19 18:00:58 +0330

    I finished it

--- python/scrapy/nini_first_scraper/nini_first_scraper/spiders/namzadiBot.py
+++ python/scrapy/nini_first_scraper/nini_first_scraper/spiders/namzadiBot.py
@@ -4,10 +4,11 @@ import scrapy
 
 class NamzadibotSpider(scrapy.Spider):
     name = 'namzadiBot'
-    allowed_domains = ['https://www.ninisite.com/discussion/forum/115/%d9%86%d8%a7%d9%85%d8%b2%d8%af%db%8c']
+    #allowed_domains = ['https://www.ninisite.com/discussion/forum/115/%d9%86%d8%a7%d9%85%d8%b2%d8%af%db%8c']
     start_urls = ['https://www.ninisite.com/discussion/forum/115/%d9%86%d8%a7%d9%85%d8%b2%d8%af%db%8c/']
 
     def parse(self, response):
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1")
         titles = response.css('.topic_subject::text').extract()
         author = response.css('.last-topic-user a::text').extract()
 
@@ -17,3 +18,11 @@ class NamzadibotSpider(scrapy.Spider):
                     'author':item[1]
                     }
             yield scraped_info
+
+        next_page = response.css('.page-link[title="Next page"]::attr(href)').extract_first()
+        if next_page:
+            print(next_page)
+            yield response.follow(
+                next_page,
+                callback=self.parse
+                )

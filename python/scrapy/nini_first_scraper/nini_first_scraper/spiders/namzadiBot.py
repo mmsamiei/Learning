383ae305bf6918b8833bf058e2699b8db2f3e780
mmsamiei <mohmahsamiei@gmail.com>
2017-12-20 23:20:33 +0330

    fix nini spider

--- python/scrapy/nini_first_scraper/nini_first_scraper/spiders/namzadiBot.py
+++ python/scrapy/nini_first_scraper/nini_first_scraper/spiders/namzadiBot.py
@@ -8,15 +8,15 @@ class NamzadibotSpider(scrapy.Spider):
     start_urls = ['https://www.ninisite.com/discussion/forum/115/%d9%86%d8%a7%d9%85%d8%b2%d8%af%db%8c/']
 
     def parse(self, response):
-        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1")
         titles = response.css('.topic_subject::text').extract()
         author = response.css('.last-topic-user a::text').extract()
-
-        for item in zip(titles, author):
+        link = response.css('.topic--title').xpath('..').xpath('./@href').extract()
+        for item in zip(titles, author, link):
             scraped_info = {
                     'titles':item[0],
-                    'author':item[1]
-                    }
+                    'author':item[1],
+                    'link':"https://www.ninisite.com"+item[2]
+                    }    
             yield scraped_info
 
         next_page = response.css('.page-link[title="Next page"]::attr(href)').extract_first()

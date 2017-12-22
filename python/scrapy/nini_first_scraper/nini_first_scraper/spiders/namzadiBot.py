4e30d8a29cbf815236ddade8d0487c753cdaba28
mmsamiei <mohmahsamiei@gmail.com>
2017-12-22 11:50:38 +0330

    k

--- python/scrapy/nini_first_scraper/nini_first_scraper/spiders/namzadiBot.py
+++ python/scrapy/nini_first_scraper/nini_first_scraper/spiders/namzadiBot.py
@@ -5,7 +5,8 @@ import scrapy
 class NamzadibotSpider(scrapy.Spider):
     name = 'namzadiBot'
     #allowed_domains = ['https://www.ninisite.com/discussion/forum/115/%d9%86%d8%a7%d9%85%d8%b2%d8%af%db%8c']
-    start_urls = ['https://www.ninisite.com/discussion/forum/115/%d9%86%d8%a7%d9%85%d8%b2%d8%af%db%8c/']
+    start_urls = ['https://www.ninisite.com/discussion/forum/115/%d9%86%d8%a7%d9%85%d8%b2%d8%af%db%8c/',
+    'https://www.ninisite.com/discussion/forum/114/%d8%ae%d8%a7%d8%b7%d8%b1%d8%a7%d8%aa-%d8%a7%d9%93%d8%b4%d9%86%d8%a7%d9%8a%d9%94%db%8c']
 
     def parse(self, response):
         titles = response.css('.topic_subject::text').extract()
